8b22f75edc4be6085344ebc89e3479719fff3fda
carlos gonzalez <carlosm1979@gmail.com>
2022-01-06 00:02:17 +0100

    api by post

--- index.js
+++ index.js
@@ -6,36 +6,27 @@ const { NodeHtmlMarkdown, NodeHtmlMarkdownOptions }   =require('node-html-markdo
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 const port = 3000
 
 app.get('/', (req, res) => {
-
   res.contentType('text/html')
   res.sendFile(path.join(__dirname, '/index.html'));
-
- 
 })
 
-
-
-app.get('/api', (req, res) => {
-  console.log(req.query.url)
-  request(req.query.url, function (error, response, body) {
+app.post('/api', (req, res) => {
+  request(req.body.url, function (error, response, body) {
     // console.log(body)
     // console.error('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('body:', body); // Print the HTML for the Google homepage.
     const $ = cheerio.load(body);
-    // console.log($('article').html())
-    // console.log(NodeHtmlMarkdown.translate($('body').html()))
     if (req.query.type === 'html') {
       res.send($('body').html())
     } else {
       res.send(NodeHtmlMarkdown.translate($('body').html()))
     }
-    // res.send(NodeHtmlMarkdown.translate($('body').html()))
-    // res.send($('body').html())
   });
   
 })
