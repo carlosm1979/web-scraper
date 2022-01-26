f60207488b3b8b20fe1b36af26935ec09d0dd9ac
carlos gonzalez <carlosm1979@gmail.com>
2022-01-26 20:45:03 +0100

    fetch static url

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
   console.log('DOMContentLoaded');
   let getButton  = document.getElementById('get_consent')
   getButton.addEventListener("click", async () => {
-    console.log('click button')
-  //   fetch("https://www.google.com").then(r => console.log(r)).catch(() => console.log('error'))
+    fetch("https://www.google.com").then(r => console.log(r)).catch(() => console.log('error'))
   });
 });
