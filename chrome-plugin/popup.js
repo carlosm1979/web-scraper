d2c502d7d2be60dd99975105695bbbe1c93634f8
carlos gonzalez <carlosm1979@gmail.com>
2022-01-26 20:43:22 +0100

    get content button event

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,9 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOMContentLoaded');
-  // let getButton  = document.getElementById('get_consent')
-  // getButton.addEventListener("click", async () => {
-  //   console.log('click button')
+  let getButton  = document.getElementById('get_consent')
+  getButton.addEventListener("click", async () => {
+    console.log('click button')
   //   fetch("https://www.google.com").then(r => console.log(r)).catch(() => console.log('error'))
-  // });
+  });
 });
