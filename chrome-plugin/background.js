4971d3390afe960cc54b92fef89e532b0e83009a
carlos gonzalez <carlosm1979@gmail.com>
2022-01-26 21:07:22 +0100

    no install layout

--- chrome-plugin/background.js
+++ chrome-plugin/background.js
@@ -1,7 +1,7 @@
 
 chrome.runtime.onInstalled.addListener(async () => {
   console.log('Test plugin');
-  let url = chrome.runtime.getURL("hello.html");
+  // let url = chrome.runtime.getURL("hello.html");
 
   // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(async () => {
   // or return a promise. Since we're inside an async function, we can await the resolution of the
   // promise returned by the tabs.create call. See the following link for more info on async/await.
   // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
-  await chrome.tabs.create({ url });
+  // await chrome.tabs.create({ url });
 
   // Finally, let's log the ID of the newly created tab using a template literal.
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
